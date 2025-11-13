c3aa314f9bf75c0a730ff86df5730b0ba09a7a85
SonDePoisson <clement.poissoncornu@gmail.com>
2025-11-13 16:16:40 +0100

    Add Argparse for simulation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import math
 import time
 import threading
+import argparse
 import mujoco
 import mujoco.viewer
 from so101_driver.so101_driver import SO101Driver
@@ -19,24 +20,42 @@ def control_loop(driver: SO101Driver, data, stop_flag):
         for id in driver.servo_ids:
             target = mujoco_to_servo(data.qpos[id - 1])
             driver.move_servo(id, target, wait=False)
-        time.sleep(1 / 60)  # Hz
+        time.sleep(1 / 60)  # 60 Hz
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="main.py",
+        description="Launch SO101 Robot in Mujoco with real robot or just simulation",
+    )
+    parser.add_argument(
+        "--simulation",
+        action="store_true",
+        help="Run only the Mujoco simulation without controlling the real robot",
+    )
+    args = parser.parse_args()
+
     model = mujoco.MjModel.from_xml_path(SCENE_PATH)
     data = mujoco.MjData(model)
-    driver = SO101Driver(SO101_PORT)
-    print("Detected servo IDs:", driver.servo_ids)
 
-    stop_flag = threading.Event()
-    thread = threading.Thread(target=control_loop, args=(driver, data, stop_flag))
-    thread.start()
+    driver = None
+    if not args.simulation:
+        driver = SO101Driver(SO101_PORT)
+        print("Detected servo IDs:", driver.servo_ids)
+        stop_flag = threading.Event()
+        thread = threading.Thread(target=control_loop, args=(driver, data, stop_flag))
+        thread.start()
+    else:
+        print("So101 Simulation")
+        stop_flag = None
+        thread = None
 
     with mujoco.viewer.launch_passive(model, data) as viewer:
         while viewer.is_running():
             mujoco.mj_step(model, data)
             viewer.sync()
 
-    stop_flag.set()
-    thread.join()
-    driver.free_robot()
+    if driver:
+        stop_flag.set()
+        thread.join()
+        driver.free_robot()
