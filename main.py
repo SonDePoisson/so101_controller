2aa528e458ae025dc7f769369e7ec769d7b544c1
SonDePoisson <clement.poissoncornu@gmail.com>
2025-11-13 15:42:58 +0100

    Control 1 servo with Mujoco

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
+import os
+import math
+import time
+import threading
 import mujoco
 import mujoco.viewer
+from so101_driver.so101_driver import SO101Driver
 
 SCENE_PATH = "SO-ARM100/Simulation/SO101/scene.xml"
-SO101_PATH = "SO-ARM100/Simulation/SO101/so101_new_calib.xml"
+SO101_PORT = os.getenv("SO101_PORT")
+
+
+def mujoco_to_servo(angle_rad):
+    # Conversion rad -> [0, 4096]
+    return int((angle_rad / math.pi) * 2048 + 2048)
+
+
+def control_loop(driver, data, stop_flag):
+    while not stop_flag.is_set():
+        # Exemple sur un seul joint
+        target = mujoco_to_servo(data.qpos[0])
+        driver.move_servo(1, target, wait=False)
+        time.sleep(0.02)  # 50 Hz
+
 
 if __name__ == "__main__":
     model = mujoco.MjModel.from_xml_path(SCENE_PATH)
     data = mujoco.MjData(model)
+    driver = SO101Driver(SO101_PORT)
+    print("Detected servo IDs:", driver.servo_ids)
+
+    stop_flag = threading.Event()
+    thread = threading.Thread(target=control_loop, args=(driver, data, stop_flag))
+    thread.start()
 
     with mujoco.viewer.launch_passive(model, data) as viewer:
         while viewer.is_running():
             mujoco.mj_step(model, data)
             viewer.sync()
+
+    stop_flag.set()
+    thread.join()
+    driver.free_robot()
