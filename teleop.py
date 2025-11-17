06005617cacf1dbe4ebdc45f15e19f8015e64beb
SonDePoisson <clement.poissoncornu@gmail.com>
2025-11-17 14:30:19 +0100

    Change rotation toggle key for space key

--- teleop.py
+++ teleop.py
@@ -1,3 +1,4 @@
+import mujoco
 from mink.contrib import TeleopMocap, keycodes
 
 
@@ -7,7 +8,12 @@ class Teleop(TeleopMocap):
     mais change la touche de bascule rotation (KEY_PERIOD → KEY_SPACE).
     """
 
-    def __init__(self, data):
+    def __init__(self, data: mujoco.MjData):
         super().__init__(data)
 
         self.actions[keycodes.KEY_SPACE] = self.toggle_rotation
+
+        if keycodes.KEY_PERIOD in self.actions:
+            del self.actions[keycodes.KEY_PERIOD]
+
+        self.toggle_on()
